da6f6ff8da9468c17ac895724b371305cb81e8d6
DarkWolf_ <whitejaguar08@gmail.com>
2022-04-30 17:50:54 +0300

    Task1-homework

---
+++ HomeWork_task1/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая 
+ на выход принимает два числа
+ и выдаёт, какое число большее,
+ а какое меньшее. */
+
+
+Console.WriteLine(" введите целое число a:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine(" введите целое число b:");
+int num2 = int.Parse(Console.ReadLine());
+int max = num1;
+if (num1 > num2);
+int min = num2;
+if (num2 < num1);
+Console.WriteLine("Максимальное число");
+Console.WriteLine(max);
+Console.WriteLine("Минимальное число");
+Console.WriteLine(min);
