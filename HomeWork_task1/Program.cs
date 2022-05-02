ad25c5448c3237a7b0f3ecc4176a9a31cc77325a
DarkWolf_ <whitejaguar08@gmail.com>
2022-05-02 16:03:41 +0300

    task1

--- HomeWork_task1/Program.cs
+++ HomeWork_task1/Program.cs
@@ -3,16 +3,25 @@
  и выдаёт, какое число большее,
  а какое меньшее.   */
 
-Console.WriteLine(" введите целое число a:");
-int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine(" введите целое число b:");
-int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите целое число a:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите целое число b:");
+int num2 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
-if (num1 > num2);
 int min = num2;
-if (num2 < num1);
+if (num1 > num2) { min = num2; max = num1; }
+{
+
 Console.WriteLine("Максимальное число");
 Console.WriteLine(max);
+    
+    if (num2 < num1) { max = num1; }
+    { 
+    
 Console.WriteLine("Минимальное число");
 Console.WriteLine(min);
+
+
+    }
 Console.ReadKey();
+    }
